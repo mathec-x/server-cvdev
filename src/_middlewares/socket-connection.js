659beb629c958ddc8583e2efe728bdb95d53b07b
matheus correa <mathec@live.com>
2022-02-27 11:27:09 -0300

    feat: subscription user

--- src/_middlewares/socket-connection.js
+++ src/_middlewares/socket-connection.js
@@ -24,6 +24,22 @@ const socketConnection = (socket) => {
         socket.emit('dispatch', { type: 'user:mount', payload: {} });
         socket.emit('dispatch', { type: 'candidates:mount', payload: [] });
     }
+
+    socket.on('subscribe', (nick) => {
+        console.log('subscribe to', nick);
+        db.candidate.findFirst({
+            where: {nick},
+            select: md.candidates.select
+        }).then( candidate => {
+            socket.join(candidate.nick);
+            socket.emit('subscription', candidate)
+        })
+    })
+
+    socket.on('unsubscribe', (nick) => {
+        console.log('unsubscribe to', nick);
+        socket.leave(nick);
+    })
 }
 
 module.exports = socketConnection;
