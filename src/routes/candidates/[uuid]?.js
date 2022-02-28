321dacc53319b6d32308530977ee13ba75d1442b
matheus correa <mathec@live.com>
2022-02-28 14:32:32 -0300

    fix: clean code

--- src/routes/candidates/[uuid]?.js
+++ src/routes/candidates/[uuid]?.js
@@ -1,21 +1,16 @@
 const db = require("../../../prisma");
 const { candidates } = require("../../../prisma/selectors");
+const { validateBody } = require("./_helpers");
 
 /**
  * @type { import("express-next-api").NextApi<{uuid: string}, import('@prisma/client').Prisma.CandidateUpdateArgs['data'] > } 
  */
  exports.put = async (req, res) => {
     try {
-        const set = {};
-            set.image = req.body.image || undefined;
-            set.name = req.body.name || undefined;
-            set.nick = req.body.nick || undefined;
-            set.email = req.body.email || undefined;
-
         const data = await db.candidate.update({
             select: candidates.select,
             where: { uuid: req.params.uuid },
-            data: set
+            data: validateBody(req.body)
         });
 
         res.to(data.nick).$emit('dispatch', { type: 'candidate:mount', payload: data });
@@ -37,11 +32,7 @@ exports.post = async (req, res) => {
             select: candidates.select,
             data: {
                 ...req.body,
-                user: {
-                    connect: {
-                        uuid: req.user.uuid
-                    }
-                }
+                user: {connect: { uuid: req.user.uuid }}
             },
         });
 
@@ -50,6 +41,21 @@ exports.post = async (req, res) => {
 
     } catch (error) {
         console.log(error)
+        res.sendStatus(400);
+    }
+}
+
+/**
+ * @type { import("express-next-api").NextApi<{uuid: string}> } 
+ */
+ exports.del = async (req, res) => {
+    try {
+        const data = await db.candidate.delete({ where: { uuid: req.params.uuid } });
+        res.to(data.nick).$emit('dispatch', { type: 'candidate:delete', payload: data });
+        res.to(req.user.uuid).$emit('dispatch', { type: 'candidates:update', payload: data });
+        return res.sendStatus(200);
+    } catch (error) {
+        console.log(error);
         res.sendStatus(400);
     }
 }
---
+++ src/routes/candidates/_helpers.js
@@ -0,0 +1,9 @@
+
+exports.validateBody = (req) => {
+    return {
+        image: req.image || undefined,
+        name: req.name || undefined,
+        nick: req.nick || undefined,
+        email: req.email || undefined,
+    }
+}
