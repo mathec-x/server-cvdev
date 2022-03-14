6588d688a57f20d7bde3c5bc7f8f947f47e46223
matheus correa <mathec@live.com>
2022-03-14 10:33:41 -0300

    fix: deploy test pwa install

--- public/service-worker.js
+++ public/service-worker.js
@@ -1,5 +1,5 @@
 /* eslint-disable no-restricted-globals */
-const CACHE_NAME = 'jep89ynDtT';
+const CACHE_NAME = 'cZyX98oDVk';
 const urlsToCache = [
   "/",
   "/assets/example_typings_2.png",
@@ -9,7 +9,7 @@ const urlsToCache = [
   "/index.html",
   "/service-worker.js",
   "/static/css/main.085c26d0.css",
-  "/static/js/main.3314eced.js"
+  "/static/js/main.c02ab5cd.js"
 ];
 // const appIcon = 'default_icon_link';
 
