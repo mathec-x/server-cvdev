7b3a38cd2f04b00544e241eb7cd3d6972a1e32d8
matheus correa <mathec@live.com>
2022-04-18 20:51:10 -0300

    feat: release features

--- public/service-worker.js
+++ public/service-worker.js
@@ -1,5 +1,5 @@
 /* eslint-disable no-restricted-globals */
-const CACHE_NAME = 'lkWmJrKM4H';
+const CACHE_NAME = 'aMDsmlFbty';
 const urlsToCache = [
   "/",
   "/assets/background.png",
@@ -127,7 +127,7 @@ const urlsToCache = [
   "/index.html",
   "/service-worker.js",
   "/static/css/main.0e9ccb94.css",
-  "/static/js/main.7ac00b24.js"
+  "/static/js/main.11adfa48.js"
 ];
 // const appIcon = 'default_icon_link';
 
